33a3c5f11195f143fc39addad25aaafd847475d9
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-02-26 17:47:10 -0600

    26 feb

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task')
+
 router.get('/', (req, res) => {
-    res.send('Hola Mundo')
+    const task = await Task.find();
+    console.log(task);
 });
 
-router.post('/add', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+router.post('/add', async (req, res) => {
+    // console.log(new Task(req.body));
+    // console.log(req.body);
+
+    const task = new Task(req.body);
+    await task.save();
+
+    res.send('Recieved');
+});
+
+router.get('/delete/:id', async (req, res) => {
+
+    const { id } = req.params;
+    Task.remove({_id: id});
+    
+    // console.log(new Task(req.body));
+    // console.log(req.body);
+
+    const task = new Task(req.body);
+    await task.save();
+
+    res.send('Recieved');
 });
 
 module.exports = router;
